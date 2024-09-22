1e520f7cda8d7097f202f31cccbe4787e4596e82
nathanljones <nathan.l.jones+github@protonmail.com>
2024-09-22 16:19:45 +0100

    Has two paddles correctly positioned to screen resolution

--- src/main.rs
+++ src/main.rs
@@ -136,23 +136,38 @@ fn spawn_paddles(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    window: Query<&Window>,
 ) {
     println!("Spawning paddles...");
 
-    let mesh = Mesh::from(Rectangle::new(PADDLE_WIDTH, PADDLE_HEIGHT));
-    let material = ColorMaterial::from(Color::srgb(0., 1., 0.));
+    if let Ok(window) = window.get_single() {
+        let window_width = window.resolution.width();
+        let padding = 50.;
+        let right_paddle_x = window_width / 2. - padding;
+        let left_paddle_x = -window_width / 2. + padding;
 
-    let mesh_handle = meshes.add(mesh);
-    let material_handle = materials.add(material);
+        let mesh = Mesh::from(Rectangle::new(PADDLE_WIDTH, PADDLE_HEIGHT));
 
-    commands.spawn((
-        PaddleBundle::new(20., -25.),
-        MaterialMesh2dBundle {
-            mesh: mesh_handle.into(),
-            material: material_handle,
-            ..default()
-        },
-    ));
+        let mesh_handle = meshes.add(mesh);
+
+        commands.spawn((
+            PaddleBundle::new(right_paddle_x, 0.),
+            MaterialMesh2dBundle {
+                mesh: mesh_handle.clone().into(),
+                material: materials.add(ColorMaterial::from(Color::srgb(0., 1., 0.))),
+                ..default()
+            },
+        ));
+
+        commands.spawn((
+            PaddleBundle::new(left_paddle_x, 0.),
+            MaterialMesh2dBundle {
+                mesh: mesh_handle.into(),
+                material: materials.add(ColorMaterial::from(Color::srgb(0., 0., 1.))),
+                ..default()
+            },
+        ));
+    }
 }
 // Returns `Some` if `ball` collides with `wall`
 // The returned `Collision` is the side of `wall`
