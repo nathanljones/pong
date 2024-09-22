c83bda6285ff49f76c251f2bf57eeb9e3dd205a8
nathanljones <nathan.l.jones+github@protonmail.com>
2024-09-22 15:44:14 +0100

    Add a paddle - only display on screen - collision handling not implemented

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use bevy::sprite::MaterialMesh2dBundle;
 
 const BALL_SIZE: f32 = 5.;
 
+const PADDLE_SPEED: f32 = 1.;
+const PADDLE_WIDTH: f32 = 10.;
+const PADDLE_HEIGHT: f32 = 50.;
+
 #[derive(Component)]
 struct Position(Vec2);
 
@@ -24,11 +28,30 @@ impl BallBundle {
     }
 }
 
+#[derive(Component)]
+struct Paddle;
+
+#[derive(Bundle)]
+struct PaddleBundle {
+    paddle: Paddle,
+    position: Position,
+}
+
+impl PaddleBundle {
+    fn new(x: f32, y: f32) -> Self {
+        Self {
+            paddle: Paddle,
+            position: Position(Vec2::new(x, y)),
+        }
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_ball)
         .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, spawn_paddles)
         .add_systems(Update, move_ball)
         // Add our projection system to run after
         // we move our ball so we are not reading
@@ -80,4 +103,27 @@ fn move_ball(
 ) {
     let mut position = ball.get_single_mut().expect("needs a single");
     position.0.x += 1.0;
+}
+
+fn spawn_paddles(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    println!("Spawning paddles...");
+
+    let mesh = Mesh::from(Rectangle::new(PADDLE_WIDTH, PADDLE_HEIGHT));
+    let material = ColorMaterial::from(Color::srgb(0., 1., 0.));
+
+    let mesh_handle = meshes.add(mesh);
+    let material_handle = materials.add(material);
+
+    commands.spawn((
+        PaddleBundle::new(20., -25.),
+        MaterialMesh2dBundle {
+            mesh: mesh_handle.into(),
+            material: material_handle,
+            ..default()
+        },
+    ));
 }
